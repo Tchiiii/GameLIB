105ba51d72679dcfc891d1932e9c8c5d6175666b
Tchi <tchiquito.tchiquito@gmail.com>
2020-12-06 00:56:54 +0100

    Exemple de card

--- site/js/data.js
+++ site/js/data.js
@@ -34,3 +34,19 @@ function ShowGameName() {
 		document.getElementById('game-list').innerHTML += game[i].name + " ";
 	}
 }
+
+
+// Récupère la variable dans l'url
+var $_GET = {};
+if(document.location.toString().indexOf('?') !== -1) {
+	var query = document.location
+						.toString()
+						.replace(/^.*?\?/, '')
+						.replace(/#.*$/, '')
+						.split('&');
+
+	for(var i=0, l=query.length; i<l; i++) {
+		var aux = decodeURIComponent(query[i]).split('=');
+		$_GET[aux[0]] = aux[1];
+	}
+} 
