e6fe6690b205c86bf6c7036dbf2768b9f5ffa800
Tchi <tchiquito.tchiquito@gmail.com>
2020-12-05 23:22:52 +0100

    Remplace URL par SessionStorage
    Remplace la mise en place sur LightMode de l'URL par le SessionStorage

--- site/js/lightmode.js
+++ site/js/lightmode.js
@@ -1,35 +1,34 @@
+// Récupère le boutton 'Night mode'
 let text = document.getElementById('light-mode');
 
-// Récupère la variable dans l'url
-var $_GET = {};
-if(document.location.toString().indexOf('?') !== -1) {
-    var query = document.location
-                   .toString()
-                   .replace(/^.*?\?/, '')
-                   .replace(/#.*$/, '')
-                   .split('&');
-
-    for(var i=0, l=query.length; i<l; i++) {
-       var aux = decodeURIComponent(query[i]).split('=');
-       $_GET[aux[0]] = aux[1];
-    }
-}
-//console.log($_GET['lightmode']);
-
-if ($_GET['darkmode'] == 'true') {
-		document.getElementById('body').className = 'dark-scheme';
-		text.innerHTML = 'Mode jour';
-} else {
+//Enlève le mode Sombre
+function SetLightMode() {
 	document.getElementById('body').className = 'light-scheme';
-	text.innerHTML = 'Mode nuit';
+	text.innerHTML = 'Mode jour';
 }
 
-function LightMode() {
-	if (document.getElementById('body').className == 'light-scheme') {
+//Met la page en mode Sombre
+function SetNightMode() {
 		document.getElementById('body').className = 'dark-scheme';
-		text.innerHTML = 'Mode jour';
-	} else {
-		document.getElementById('body').className = 'light-scheme';
 		text.innerHTML = 'Mode nuit';
+}
+
+// Demarrage de la page
+function LightModeOnLoad() {
+	if (sessionStorage.getItem('nightMode') == "true") {
+		SetNightMode();
+	} else {
+		SetLightMode();
+	}
+}
+
+// Boutton
+function LightModeButton() {
+	if (document.getElementById('body').className == 'dark-scheme') {
+		SetLightMode();
+		sessionStorage.setItem('nightMode', "false");
+	} else {
+		SetNightMode();
+		sessionStorage.setItem('nightMode', "true");
 	}
 }
