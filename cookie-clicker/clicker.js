4cc01e10a140d6bcec0c743570feb36351729417
Tchi <tchiquito.tchiquito@gmail.com>
2020-12-07 22:53:30 +0100

    Edit Coockie Clicker Decrease Lags

--- cookie-clicker/clicker.js
+++ cookie-clicker/clicker.js
@@ -4,7 +4,7 @@ let upgrades = [
 						"name": "Auto-click",
 						"price": 15,
 						"level": 0,
-						"number": 1
+						"number": 0
 					}
 				]
 var interval;
@@ -12,34 +12,39 @@ var interval;
 function Initialisation() {
 	document.getElementById("price-1").innerHTML = upgrades[0].price;
 	document.getElementById("level-1").innerHTML = upgrades[0].level;
-	document.getElementById("persecond").innerHTML = (upgrades[0].number -1) + '/sec';
+	document.getElementById("persecond").innerHTML = (upgrades[0].number) + '/sec';
 }
 
-function OneCookie() {
-	score++;
+function OneCookie(number) {
+	score += number;
 	document.getElementById('score').innerText = score;
 }
 
-function Buy(price, count, sec) {
+function Buy(price) {
 	if (score >= price) {
 		score -= price;
 		document.getElementById('score').innerText = score;
-		Autoclick(count, sec);
 
 		upgrades[0].price *= 2;
 		upgrades[0].level += 1;
-		upgrades[0].number *= 2;
+		if (upgrades[0].number == 0) {
+			upgrades[0].number = 1;
+		} else {
+			upgrades[0].number *= 2;
+		}
+
+		Autoclick(upgrades[0].number);
 		document.getElementById("icon-1").innerHTML = '<img src="icon.webp" width="25px" height="25px" onclick="Buy('
 													  + upgrades[0].price
 													  + ',' + upgrades[0].number
-													  + ',1)">';
+													  + ')">';
 		Initialisation();
 	} else {
 		console.log("Pas assez de cookies !")
 	}
 }
 
-function Autoclick(count, sec) {
+function Autoclick(count) {
 	clearInterval(interval);
-	interval = setInterval("OneCookie()", (sec / count) * 1000 );
+	interval = setInterval("OneCookie(" + count + ")", 1000 );
 }
