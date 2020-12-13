da820f0f29bb25d5ec1cda04c48545217956945d
Tchi <tchiquito.tchiquito@gmail.com>
2020-12-13 21:42:52 +0100

    Edit Menu + logo, Edit Cards

--- groupe1/js/lightmode.js
+++ groupe1/js/lightmode.js
@@ -3,18 +3,25 @@
  * @author Vabre Lucas
  */
 
-// 
+// Recupère les 
 let lightModeImg = document.getElementById('light-mode');	//Récupère le boutton 'Night mode'
-let pathSun = "img/icons/sun.svg";	//Récupère le chemin de l'iconne de soleil
-let pathMoon = "img/icons/moon.svg";	//Récupère le chemin de l'iconne de lune
+let logoImg = document.getElementById('logo');				//Récupère la balise img du logo
+
+// Recupère les images
+let sun = "img/icons/sun.svg";					//Récupère le chemin de l'iconne de soleil
+let moon = "img/icons/moon.svg";				//Récupère le chemin de l'iconne de lune
+let logoLight = "img/icons/logo-light.svg";		//Récupère le chamin du logo en mode jour
+let logoNight = "img/icons/logo-night.svg";		//Récupère le chamin du logo en mode nuit
 
 //Met la page en mode Clair
 function SetLightMode(index) { //Index permet de savoir si la page actuelle est index.html ou pas
 	document.getElementById('body').className = 'light-theme';
 	if (index) {
-		lightModeImg.setAttribute("src", pathSun);
+		lightModeImg.setAttribute("src", sun);
+		logoImg.setAttribute("src", logoLight);
 	} else {
-		lightModeImg.setAttribute("src", "../" + pathSun);
+		lightModeImg.setAttribute("src", "../" + sun);
+		logoImg.setAttribute("src", "../" + logoLight);
 	}
 	
 }
@@ -23,9 +30,11 @@ function SetLightMode(index) { //Index permet de savoir si la page actuelle est 
 function SetNightMode(index) {
 		document.getElementById('body').className = 'dark-theme';
 		if (index) {
-			lightModeImg.setAttribute("src", pathMoon);
+			lightModeImg.setAttribute("src", moon);
+			logoImg.setAttribute("src", logoNight);
 		} else {
-			lightModeImg.setAttribute("src", "../" + pathMoon);
+			lightModeImg.setAttribute("src", "../" + moon);
+			logoImg.setAttribute("src", "../" + logoNight);
 		}
 		
 }
