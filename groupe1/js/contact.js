514b20cd40571fd3e2e7300744de2ecdea6d8df3
Lavix31 <75038852+Lavix31@users.noreply.github.com>
2020-12-31 11:35:29 +0100

    contact.html
    - Mise à jour du formulaire

--- groupe1/js/contact.js
+++ groupe1/js/contact.js
@@ -1,4 +1,16 @@
-function msg () 
-    { 
-    alert( "Votre demande a bien été enregistré."); 
-    } 	
+/*
+ * contact.js
+ * @author Lilian Strub
+ */
+
+first_input.focus();
+soumis = false;
+function myFunction () { 
+    soumis = true;
+    document.forms[0].submit(); 
+}
+
+function msg () { 
+    if (!soumis)
+    	return "Votre demande a bien été envoyée.\nMerci !";
+}
