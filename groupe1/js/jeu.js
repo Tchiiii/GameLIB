f4c84078bff874189233176d93cba1b6debb8d28
Tchi <tchiquito.tchiquito@gmail.com>
2021-01-03 17:50:01 +0100

    Resolu le prob du nom + debut de modif du js

--- groupe1/js/jeu.js
+++ groupe1/js/jeu.js
@@ -8,20 +8,51 @@
  */
 
 
-/* Fonction qui récupère les variables/valeurs dans l'URL */
-let $_GET = {};	// Creer l'objet GET
-if(document.location.toString().indexOf('?') != -1) {	// Verifie si il y a un ? dans l'url
-	var query = document.location	// Modifie quelques caractères qui pourrais être mal interprété
-						.toString()
-						.replace(/^.*?\?/, '')
-						.replace(/#.*$/, '')
-						.split('&');
+/**
+ * Fonction qui récupère
+ * les variables/valeurs dans l'URL
+ */
+function GetUrl() {	
+	let $_GET = {};	// Creer l'objet GET
+	if(document.location.toString().indexOf('?') != -1) {	// Verifie si il y a un ? dans l'url
+		var query = document.location	// Modifie quelques caractères qui pourrais être mal interprété
+							.toString()
+							.replace(/^.*?\?/, '')
+							.replace(/#.*$/, '')
+							.split('&');
 
-	/* Tout ce qu'il se trouve apres le ? va etre traité */
-	for(var i=0, l=query.length; i<l; i++) {
-		var aux = decodeURIComponent(query[i]).split('=');	// Ce qu'il y a gauche de l'égal est le nom de la variable
-		$_GET[aux[0]] = aux[1];								// et ce qu'il a a droite est la valeur de celle-ci
+		/* Tout ce qu'il se trouve apres le ? va etre traité */
+		for(var i=0, l=query.length; i<l; i++) {
+			var aux = decodeURIComponent(query[i]).split('=');	// Ce qu'il y a gauche de l'égal est le nom de la variable
+			$_GET[aux[0]] = aux[1];								// et ce qu'il a a droite est la valeur de celle-ci
+		}
 	}
+}
+
+/**
+ * Fonction qui calcule la promotion
+ * @param {Double} promoValue : Le pourcentage de la promotion
+ * @param {int} price 		  : Le prix original de la promo
+ */
+function CalcPromo(promoValue, price) {
+	return ((1 - promoValue / 100) * price ).toFixed(2)
+}
+
+/**
+ * Créer plus facilement et lisiblement une balise <tr></tr>
+ * @param {String} argument : Contenu du tr
+ * @param {String} style    : Style utilisé
+ */
+function MakeTr(argument, style) {
+	return ('<tr style="' + style + '"">' + argument + '</tr>');
+}
+
+/**
+ * Créer plus facilement et lisiblement une balise <td></td>
+ * @param {String} argument : Contenu du td
+ */
+function MakeTd(argument) {
+	return ('<td>' + argument + '</td>');
 }
 
 /**
@@ -31,33 +62,47 @@ if(document.location.toString().indexOf('?') != -1) {	// Verifie si il y a un ? 
  * element de la page
  */
 function PageJeu () {
+
+	/* Initialisation des variables */
+	GetUrl();
 	let gameElement = game[parseInt($_GET["id"])];
 
 	/* Elements textes */
-	document.getElementById("game-title-0").innerHTML += gameElement.name ;
-	document.getElementById("game-title-1").innerHTML = gameElement.name ;
-	document.getElementById("game-title-2").innerHTML = gameElement.name ;
-	document.getElementById("game-description").innerHTML = gameElement.info ;
-	document.getElementById("dev").innerHTML = gameElement.developper;
-	document.getElementById("editor").innerHTML = gameElement.editor;
-	document.getElementById("pegi").innerHTML = gameElement.classification;
+	let title1 = document.getElementById("game-title-0").innerHTML;
+	let title2 = document.getElementById("game-title-1").innerHTML;
+	let title3 = document.getElementById("game-title-2").innerHTML;
+	let description = document.getElementById("game-description").innerHTML;
+	let developper = document.getElementById("dev").innerHTML;
+	let editor = document.getElementById("editor").innerHTML;
+	let pegi = document.getElementById("pegi").innerHTML;
+	let price = document.getElementById("price").innerHTML;
+	let payButton = document.getElementById("pay-button").value;
 
 	/* Images */
-	document.getElementById("picture-ingame").style.backgroundImage = "url('../" + gameElement.ingame + "')";	// Image sous la vidéo
-	document.getElementById("picture-logo").style.backgroundImage = "url('../" + gameElement.logo + "')";
-	document.getElementById("video").src = "https://www.youtube.com/embed/" + gameElement.video + "?mute=1&autoplay=1";	// Lien de la video
+	let ingame = document.getElementById("picture-ingame").style.backgroundImage = "url('../" + gameElement.ingame + "')";	// Image sous la vidéo
+	let gameLogo = document.getElementById("picture-logo").style.backgroundImage = "url('../" + gameElement.logo + "')";
+	let video = document.getElementById("video").src = "https://www.youtube.com/embed/" + gameElement.video + "?mute=1&autoplay=1";	// Lien de la video
 	
+	/* Affectation des elements */
+	title1 += gameElement.name;
+	title2 = gameElement.name;
+	title3 = gameElement.name;
+	description = gameElement.info;
+	developper = gameElement.developper;
+	editor = gameElement.editor;
+	pegi = gameElement.classification;
+
 	/* Prix */
 	if (gameElement.price == 0) {
-		document.getElementById("price").innerHTML = '';
-		document.getElementById("pay-button").value = 'Jouer';
+		price = '';
+		payButton = 'Jouer';
 	} else {
 		if (gameElement.isInPromo) {
-			document.getElementById("price").innerHTML = ((1 - gameElement.promo / 100) * gameElement.price ).toFixed(2) + " €";
+			price = CalcPromo(gameElement.promo,gameElement.price) + " €";
 		} else {
-			document.getElementById("price").innerHTML = gameElement.price + " €";
+			price = gameElement.price + " €";
 		}
-		document.getElementById("pay-button").value = 'Acheter'
+		payButton = 'Acheter'
 	}
 }
 
@@ -68,46 +113,36 @@ function PageJeu () {
 * c'est pourquoi elle figure uniquement dans dev.html
 */
 function ShowGameAll() {
-	document.getElementById('game-list').innerHTML += '<tr style="background-color: var(--overlay-color);">'
-													  + '<td>id</td>'
-													  + '<td>name</td>'
-													  + '<td>price</td>'
-													  + '<td>isInPromo</td>'
-													  + '<td>promo</td>'
-													  + '<td>info</td>'
-													  + '<td>developper</td>'
-													  + '<td>editor</td>'
-													  + '<td>classification</td>'
-													  + '<td>img1</td>'
-													  + '<td>img2</td>'
-													  + '<td>img3</td>'
-													  +'</tr>';
+	document.getElementById('game-list').innerHTML = MakeTr(MakeTd('id')
+															+ MakeTd('name') 
+															+ MakeTd('price') 
+															+ MakeTd('isInPromo') 
+															+ MakeTd('promo') 
+															+ MakeTd('info') 
+															+ MakeTd('developper') 
+															+ MakeTd('editor') 
+															+ MakeTd('classification') 
+															+ MakeTd('imgHorizontal') 
+															+ MakeTd('imgVertical') 
+															+ MakeTd('ingame') 
+															+ MakeTd('video'),
+															'background-color: var(--overlay-color)'
+															);
 	for (let i = 0; i < game.length; i++) { // Affiche autant de lignes que d'elements dans la liste game
-		document.getElementById('game-list').innerHTML += '<tr><td>'
-														  + game[i].id
-														  + '</td><td>'
-														  + game[i].name
-														  +'</td> <td>'
-														  + game[i].price
-														  + '</td><td>'
-														  + game[i].isInPromo
-														  + '</td><td>'
-														  + game[i].promo
-														  + '</td><td>'
-														  + game[i].info
-														  + '</td><td>'
-														  + game[i].developper
-														  + '</td><td>'
-														  + game[i].editor
-														  + '</td><td>'
-														  + game[i].classification
-														  + '</td><td>'
-														  + game[i].imgHorizontal
-														  + '</td><td>'
-														  + game[i].imgVertical
-														  + '</td><td>'
-														  + game[i].ingame
-														  + '</td> </tr>';
+		document.getElementById('game-list').innerHTML += MakeTr(MakeTd(game[i].id)
+														  		 + MakeTd(game[i].name)
+														  		 + MakeTd(game[i].price)
+														  		 + MakeTd(game[i].isInPromo)
+														  		 + MakeTd(game[i].promo)
+														  		 + MakeTd(game[i].info)
+														  		 + MakeTd(game[i].developper)
+														  		 + MakeTd(game[i].editor)
+														  		 + MakeTd(game[i].classification)
+														  		 + MakeTd(game[i].imgHorizontal)
+														  		 + MakeTd(game[i].imgVertical)
+														  		 + MakeTd(game[i].ingame)
+														  		 + MakeTd(game[i].video));
+
 	}
 }
 
